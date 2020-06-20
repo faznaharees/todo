5b5750340159d547ebdc7ad81d158ae738db4471
Fazna <faznaharees@cet.ac.in>
2020-06-20 20:38:46 +0530

    Updates

---
+++ precache-manifest.d8484e21ce16e3c2078ff39765802a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195bc06150dbeec949032b1d089cf899",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "25ac8bbdbce77a3cda58",
+    "url": "/todo/static/css/main.606d99d5.chunk.css"
+  },
+  {
+    "revision": "7fb56fe45f21c45e46ce",
+    "url": "/todo/static/js/2.c419e185.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c419e185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ac8bbdbce77a3cda58",
+    "url": "/todo/static/js/main.c9ecbfd5.chunk.js"
+  },
+  {
+    "revision": "73306ce994a6ed0954a2",
+    "url": "/todo/static/js/runtime-main.a940b003.js"
+  }
+]);
